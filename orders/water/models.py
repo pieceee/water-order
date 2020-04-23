4b3f2892dff3f2f0b9bfe6bee91e01b679e69db4
Alexander Chirkin <chalex2000@yandex.ru>
2020-04-23 03:22:49 +0300

    API.

--- orders/water/models.py
+++ orders/water/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Product(models.Model):
@@ -37,3 +39,18 @@ class ProductOrder(models.Model):
 
     def __str__(self):
         return "{} {} {}".format(self.product_id, self.order_id, self.count)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=12)
+    name = models.CharField(max_length=50)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
